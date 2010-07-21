86afea60f136aff8f4625f4feacf6e4f545a4f60
Engleek <engleek@engleek.com>
2010-07-21 22:20:08 +0200

    Removed the demuxer node.

--- DVDAddOn.cpp
+++ DVDAddOn.cpp
@@ -15,56 +15,20 @@
 DVDAddOn::DVDAddOn(image_id imid)
     : BMediaAddOn(imid)
 {
-    // Multistream format, output for media node, input for extractor node
-
-    fMultistreamMediaFormat.type = B_MEDIA_MULTISTREAM;
-    fMultistreamMediaFormat.u.multistream = media_multistream_format::wildcard;
-    fMultistreamMediaFormat.u.multistream.format = media_multistream_format::B_MPEG2;
-
-    // Final output formats for extractor node
-    
-    // Video stream
-    fFinalStreamFormats[0].type = B_MEDIA_ENCODED_VIDEO;
-    fFinalStreamFormats[0].u.encoded_video = media_encoded_video_format::wildcard;
-
-    // Audio stream
-    fFinalStreamFormats[0].type = B_MEDIA_ENCODED_AUDIO;
-    fFinalStreamFormats[0].u.encoded_audio = media_encoded_audio_format::wildcard;
-
-    // Subs stream, don't know how to deal with this yet, so custom format
-    fFinalStreamFormats[0].type = B_MEDIA_FIRST_USER_TYPE;
-    
-    // Note: There might be a fourth data stream
-
     // The media node flavor
 
     fMediaNodeFlavorInfo.name = "DVD Disk Node";
-    fMediaNodeFlavorInfo.info = "Extracts media and navigation data from the DVD disk.";
-    fMediaNodeFlavorInfo.kinds = B_BUFFER_PRODUCER | B_CONTROLLABLE;
+    fMediaNodeFlavorInfo.info = "Media and navigation for DVD disks.";
+    fMediaNodeFlavorInfo.kinds = B_BUFFER_PRODUCER;
     fMediaNodeFlavorInfo.flavor_flags = 0;
     fMediaNodeFlavorInfo.internal_id = 0;
     fMediaNodeFlavorInfo.possible_count = 2; // Depends on number of drives really
     fMediaNodeFlavorInfo.in_format_count = 0;
     fMediaNodeFlavorInfo.in_format_flags = 0;
     fMediaNodeFlavorInfo.in_formats = NULL;
-    fMediaNodeFlavorInfo.out_format_count = 1;
+    fMediaNodeFlavorInfo.out_format_count = 0;
     fMediaNodeFlavorInfo.out_format_flags = 0;
-    fMediaNodeFlavorInfo.out_formats = &fMultistreamMediaFormat;
-
-    // The extractor node flavour
-
-    fExtractorNodeFlavorInfo.name = "DVD Extractor";
-    fExtractorNodeFlavorInfo.info = "Splits the DVD media into seperate streams.";
-    fExtractorNodeFlavorInfo.kinds = B_BUFFER_CONSUMER | B_BUFFER_PRODUCER;
-    fExtractorNodeFlavorInfo.flavor_flags = 0;
-    fExtractorNodeFlavorInfo.internal_id = 1;
-    fExtractorNodeFlavorInfo.possible_count = 2; // Depends on number of drives really
-    fExtractorNodeFlavorInfo.in_format_count = 1;
-    fExtractorNodeFlavorInfo.in_format_flags = 0;
-    fExtractorNodeFlavorInfo.in_formats = &fMultistreamMediaFormat;
-    fExtractorNodeFlavorInfo.out_format_count = 3;
-    fExtractorNodeFlavorInfo.out_format_flags = 0;
-    fExtractorNodeFlavorInfo.out_formats = fFinalStreamFormats;
+    fMediaNodeFlavorInfo.out_formats = NULL;
 
     fInitStatus = B_OK;
 }
@@ -108,9 +72,6 @@ DVDAddOn::GetFlavorAt(int32 n, const flavor_info **out_info)
     case 0:
         *out_info = &fMediaNodeFlavorInfo;
         break;
-    case 1:
-        *out_info = &fExtractorNodeFlavorInfo;
-        break;
     default:
         return B_BAD_INDEX;
         break;
@@ -130,14 +91,6 @@ DVDAddOn::InstantiateNodeFor(
 
     if (info->internal_id == fMediaNodeFlavorInfo.internal_id) {
         DVDDiskNode *node = new DVDDiskNode(this, fMediaNodeFlavorInfo.name, fMediaNodeFlavorInfo.internal_id);
-        if (node && (node->InitCheck() < B_OK)) {
-            delete node;
-            return NULL;
-        } else {
-            return node;
-        }
-    } else if (info->internal_id == fExtractorNodeFlavorInfo.internal_id) {
-        DVDDemuxerNode *node = new DVDDemuxerNode(&fExtractorNodeFlavorInfo, this);
         if (node && (node->InitCheck() < B_OK)) {
             delete node;
             return NULL;
